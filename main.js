afa2c9a9d5807178be33f98b97c8653079c60616
DarkestofTimes <133776070+DarkestofTimes@users.noreply.github.com>
2023-11-04 21:49:21 +0100

    Horrible stuff

--- main.js
+++ main.js
@@ -946,46 +946,6 @@ function styleTagTransform(css, styleElement) {
 }
 module.exports = styleTagTransform;
 
-/***/ }),
-
-/***/ "./img/Screenshot400w.png":
-/*!********************************!*\
-  !*** ./img/Screenshot400w.png ***!
-  \********************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "6c146408fcf1e6b95107.png";
-
-/***/ }),
-
-/***/ "./img/Screenshot800w.png":
-/*!********************************!*\
-  !*** ./img/Screenshot800w.png ***!
-  \********************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "295256f4a23cc727ea47.png";
-
-/***/ }),
-
-/***/ "./img/battleship400.png":
-/*!*******************************!*\
-  !*** ./img/battleship400.png ***!
-  \*******************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "8f6e484ba0cf51a0e1f3.png";
-
-/***/ }),
-
-/***/ "./img/battleship800.png":
-/*!*******************************!*\
-  !*** ./img/battleship800.png ***!
-  \*******************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "b79d407bb3d98c39a12d.png";
-
 /***/ })
 
 /******/ 	});
@@ -1039,18 +999,6 @@ module.exports = __webpack_require__.p + "b79d407bb3d98c39a12d.png";
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
 /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
@@ -1067,29 +1015,6 @@ module.exports = __webpack_require__.p + "b79d407bb3d98c39a12d.png";
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src;
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) {
-/******/ 					var i = scripts.length - 1;
-/******/ 					while (i > -1 && !scriptUrl) scriptUrl = scripts[i--].src;
-/******/ 				}
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/nonce */
 /******/ 	(() => {
 /******/ 		__webpack_require__.nc = undefined;
@@ -1104,14 +1029,6 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _src_styles_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/styles.css */ "./src/styles.css");
-/* harmony import */ var _img_battleship400_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/battleship400.png */ "./img/battleship400.png");
-/* harmony import */ var _img_battleship800_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/battleship800.png */ "./img/battleship800.png");
-/* harmony import */ var _img_Screenshot400w_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/Screenshot400w.png */ "./img/Screenshot400w.png");
-/* harmony import */ var _img_Screenshot800w_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../img/Screenshot800w.png */ "./img/Screenshot800w.png");
-
-
-
-
 
 
 const elementIsIn = (element) => {
